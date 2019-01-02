b65f4fc1be94b8958f826d6255f632572b994040
quarrier <191679972@qq.com>
2019-01-02 10:27:33 +0800

    0.10.0

---
+++ IFCAnServiceBusMdl/IFCAnServiceBusMdl/IFCAnServiceBusModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Volo.Abp.Modularity;
+
+namespace IFCAnServiceBusMdl
+{
+    public class IFCAnServiceBusModule : AbpModule
+    {
+        public override void PreConfigureServices(ServiceConfigurationContext context)
+        {
+            base.PreConfigureServices(context);
+        }
+
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            base.ConfigureServices(context);
+        }
+    }
+}
